4f0b0098d4eb26b4bea795908f299f5218399f72
mahmoudosman.som@gmail.com <mahmoudosman.som@gmail.com>
2021-01-27 20:54:04 -0600

    updated sort by links

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,25 +2,10 @@ $(document).ready(function () {
   //Bags heart
 
   $(".bags .bags__content__container .fa-heart").click(function () {
-    //$(this).css("background-color", "#000");
-    //$(".bags .bags__content__container .fa-heart").css("background-color", "#f00");
-    //alert("Clicked!");
     $(this).addClass("fas fa-heart");
   });
 
-  //Sort product
-  $(".fa-chevron-up").click(function () {
-    //$(".sort_by_links ul li").toggle();
-    // $(".sort_by_links ul li")
-    //   .addClass("fa-chevron-down")
-    //   .removeClass("fa-chevron-up")
-    //   .toggle();
-    $(".sort_by_links ul li")
-      .addClass("fa-chevron-down")
-      // .removeClass("fa-chevron-up")
-      .toggle();
-  });
-
+  //Steps script
   $(".step_one ul li a").click(function (e) {
     //alert($(this).text());
     $(this).parent().addClass("active").siblings().removeClass("active");
